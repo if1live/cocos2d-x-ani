e5c7784a7671b081b03022783ee75ac160c3889f
if1live <libsora25@gmail.com>
2013-07-13 16:00:55 +0900

    init cocos2dx

---
+++ Classes/stdafx.h
@@ -0,0 +1,12 @@
+﻿// Ŭnicode please 
+#pragma once
+
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+#include <string>
+#include <map>
+#include <algorithm>
+#include <vector>
